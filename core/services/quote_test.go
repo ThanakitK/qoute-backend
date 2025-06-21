19fab73406193bfa85bcf47502776928576f05bf
Thanakit Kitkhajohnpaiboon <thanakit.kitkh@inet.co.th>
2025-06-21 13:41:46 +0700

    add update quote test

--- core/services/quote_test.go
+++ core/services/quote_test.go
@@ -225,3 +225,260 @@ func Test_CreateQuote(t *testing.T) {
 		})
 	}
 }
+
+func Test_UpdateQuote(t *testing.T) {
+	type test struct {
+		Name  string
+		Input struct {
+			ID    string
+			Quote string
+			Vote  int
+		}
+		Mock struct {
+			UpdateQuote struct {
+				Input struct {
+					ID      string
+					Payload models.UpdateQuoteModel
+				}
+				Output models.QuoteModel
+				Error  error
+			}
+		}
+		Output models.ResponseModel
+	}
+	id := uuid.New().String()
+	quoteID := uuid.New().String()
+	date := time.Now()
+	cases := []test{
+		{
+			Name: "update quote success",
+			Input: struct {
+				ID    string
+				Quote string
+				Vote  int
+			}{
+				ID:    id,
+				Quote: quoteID,
+				Vote:  1,
+			},
+			Mock: struct {
+				UpdateQuote struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}
+			}{
+				UpdateQuote: struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}{
+					Input: struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}{
+						ID: id,
+						Payload: models.UpdateQuoteModel{
+							Quote: quoteID,
+							Vote:  1,
+						},
+					},
+					Output: models.QuoteModel{
+						ID:         id,
+						Quote:      quoteID,
+						UpdateDate: date,
+					},
+					Error: nil,
+				},
+			},
+			Output: models.ResponseModel{
+				Status:  true,
+				Code:    200,
+				Message: "update quote success",
+				Result: models.QuoteModel{
+					ID:         id,
+					Quote:      quoteID,
+					UpdateDate: date,
+				},
+			},
+		},
+		{
+			Name: "id not found",
+			Input: struct {
+				ID    string
+				Quote string
+				Vote  int
+			}{
+				ID:    "",
+				Quote: quoteID,
+				Vote:  1,
+			},
+			Mock: struct {
+				UpdateQuote struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}
+			}{
+				UpdateQuote: struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}{},
+			},
+			Output: models.ResponseModel{
+				Status:  false,
+				Code:    400,
+				Message: "id or quote not found",
+				Result:  nil,
+			},
+		},
+		{
+			Name: "quote id not found",
+			Input: struct {
+				ID    string
+				Quote string
+				Vote  int
+			}{
+				ID:    id,
+				Quote: "",
+				Vote:  1,
+			},
+			Mock: struct {
+				UpdateQuote struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}
+			}{
+				UpdateQuote: struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}{},
+			},
+			Output: models.ResponseModel{
+				Status:  false,
+				Code:    400,
+				Message: "id or quote not found",
+				Result:  nil,
+			},
+		},
+		{
+			Name: "vote must be >= 0",
+			Input: struct {
+				ID    string
+				Quote string
+				Vote  int
+			}{
+				ID:    id,
+				Quote: quoteID,
+				Vote:  -1,
+			},
+			Mock: struct {
+				UpdateQuote struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}
+			}{
+				UpdateQuote: struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}{},
+			},
+			Output: models.ResponseModel{
+				Status:  false,
+				Code:    400,
+				Message: "vote must be >= 0",
+				Result:  nil,
+			},
+		},
+		{
+			Name: "update quote error",
+			Input: struct {
+				ID    string
+				Quote string
+				Vote  int
+			}{
+				ID:    id,
+				Quote: quoteID,
+				Vote:  1,
+			},
+			Mock: struct {
+				UpdateQuote struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}
+			}{
+				UpdateQuote: struct {
+					Input struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}
+					Output models.QuoteModel
+					Error  error
+				}{
+					Input: struct {
+						ID      string
+						Payload models.UpdateQuoteModel
+					}{
+						ID: id,
+						Payload: models.UpdateQuoteModel{
+							Quote: quoteID,
+							Vote:  1,
+						},
+					},
+					Output: models.QuoteModel{},
+					Error:  errors.New("update quote error"),
+				},
+			},
+			Output: models.ResponseModel{
+				Status:  false,
+				Code:    400,
+				Message: "update quote error",
+				Result:  nil,
+			},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			quoteRepo := repositories.NewQuoteRepositoryMock()
+			quoteRepo.On("UpdateQuote", mock.Anything, mock.Anything).Return(c.Mock.UpdateQuote.Output, c.Mock.UpdateQuote.Error)
+
+			quoteService := services.NewQuoteService(quoteRepo)
+			result := quoteService.UpdateQuote(c.Input.ID, c.Input.Quote, c.Input.Vote)
+
+			assert.Equal(t, c.Output, result)
+		})
+	}
+}
